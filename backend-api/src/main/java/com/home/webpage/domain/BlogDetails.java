bedd42a3a5ddf561211a19cdab6a7dc4f5a62e6e
Pavel Usanli <pavel.usanli@gmail.com>
2026-01-25 12:50:08 +0200

    code formatting

--- backend-api/src/main/java/com/home/webpage/domain/BlogDetails.java
+++ backend-api/src/main/java/com/home/webpage/domain/BlogDetails.java
@@ -2,5 +2,4 @@ package com.home.webpage.domain;
 
 import java.time.Instant;
 
-public record BlogDetails(
-    Long id, String title, String description, Instant createdAt) {}
+public record BlogDetails(Long id, String title, String description, Instant createdAt) {}
